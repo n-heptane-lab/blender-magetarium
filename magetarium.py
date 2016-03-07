95e80592fa93f40520a477ab048f060deaa61dac
Jeremy Shaw <jeremy@n-heptane.com>
2016-03-06 22:19:14 -0600

    major updates to edge generation

--- magetarium.py
+++ magetarium.py
@@ -132,6 +132,7 @@ def calcVerts(a):
 
     return vertex
 
+# reorder vertexs to a more sensible numbering
 def domeVerts(vertex):
     vertex2 = [ vertex[42], vertex[46], vertex[34], vertex[38], vertex[50],
                 vertex[40], vertex[54], vertex[26], vertex[30], vertex[58],
@@ -144,6 +145,18 @@ def domeVerts(vertex):
               ]
     return vertex2
 
+def domeVerts_ccw(vertex):
+    vertex2 = [ vertex[42], vertex[50], vertex[38], vertex[34], vertex[46],
+                vertex[40], vertex[58], vertex[30], vertex[26], vertex[54],
+                vertex[44], vertex[48], vertex[56], vertex[10], vertex[18], vertex[23], vertex[21], vertex[14], vertex[ 6], vertex[52],
+                vertex[32], vertex[36], vertex[ 8], vertex[ 2], vertex[19], vertex[31], vertex[27], vertex[15], vertex[ 0], vertex[ 4],
+                vertex[24], vertex[28], vertex[16], vertex[ 3], vertex[11], vertex[39], vertex[35], vertex[ 7], vertex[ 1], vertex[12],
+                vertex[20], vertex[22], vertex[17], vertex[ 9], vertex[59], vertex[51], vertex[47], vertex[55], vertex[ 5], vertex[13],
+                vertex[25], vertex[29], vertex[57], vertex[43], vertex[53],
+                vertex[33], vertex[37], vertex[49], vertex[41], vertex[45],
+              ]
+    return vertex2
+
 def dupVerts(verts):
 #    origVerts = calcVerts(edgeLength/3.28084)
 #    verts = domeVerts(origVerts)
@@ -151,7 +164,250 @@ def dupVerts(verts):
     for i in range(0, len(verts)):
         dupObjectTo(i, verts[i])
 
-def dupEdges(verts):
+# list of vertexs that form the edges
+def faceList():
+              # face 1
+    faces = [ [ (0, 1),
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 0)],
+              # face 2
+              [ (0, 5),
+                (5, 11),
+                (11, 12),
+                (12, 6),
+                (6,  1),
+                (1,  0)],
+              # face 3
+              [ (1, 6),
+                (6, 13),
+                (13, 14),
+                (14, 7),
+                (7, 2),
+                (2, 1)],
+              # face 4
+              [ (2, 7),
+                (7, 15),
+                (15, 16),
+                (16, 8),
+                (8, 3),
+                (3, 2),
+                (2, 7)],
+              # face 5
+              [ (3, 8),
+                (8, 17),
+                (17, 18),
+                (18, 9),
+                (9, 4),
+                (4, 3)],
+              # face 6
+              [ (4, 9),
+                (9, 19),
+                (19, 10),
+                (10, 5),
+                (5, 0),
+                (0, 4)],
+              # face 7
+              [ (5, 10),
+                (10, 20),
+                (20, 21),
+                (21, 11),
+                (11, 5)],
+              # face 8
+              [ (6, 12),
+                (12, 22),
+                (22, 23),
+                (23, 13),
+                (13, 6)],
+              # face 9
+              [ (7, 14),
+                (14, 24),
+                (24, 25),
+                (25, 15),
+                (15, 7)],
+              # face 10
+              [ (8, 16),
+                (16, 26),
+                (26, 27),
+                (27, 17),
+                (17, 8)],
+              # face 11
+              [ (9, 18),
+                (18, 28),
+                (28, 33),
+                (33, 19),
+                (19, 9)],
+              # face 12
+              [ (11, 21),
+                (21, 31),
+                (31, 32),
+                (32, 22),
+                (22, 12)],
+              # face 13
+              [ (13, 23),
+                (23, 29),
+                (29, 34),
+                (34, 24),
+                (24, 14),
+                (14, 13)],
+              # face 14
+              [ (15, 25),
+                (25, 35),
+                (35, 36),
+                (36, 26),
+                (26, 16),
+                (16, 15)],
+              # face 15
+              [ (17, 27),
+                (27, 37),
+                (37, 38),
+                (38, 28),
+                (28, 18),
+                (18, 17)],
+              # face 16
+              [ (19, 33),
+                (33, 39),
+                (39, 30),
+                (30, 20),
+                (20, 10),
+                (10, 19)],
+              # face 17
+              [ (20, 30),
+                (30, 40),
+                (40, 41),
+                (41, 31),
+                (31, 21),
+                (21, 20)],
+              # face 18
+              [ (22, 32),
+                (32, 42),
+                (42, 43),
+                (43, 29),
+                (29, 23),
+                (23, 22)],
+              # face 19
+              [ (24, 34),
+                (34, 44),
+                (44, 45),
+                (45, 35),
+                (35, 25),
+                (25, 24)],
+              # face 20
+              [ (26, 36),
+                (36, 46),
+                (46, 47),
+                (47, 37),
+                (37, 27),
+                (27, 26) ],
+              # face 21
+              [ (28, 38),
+                (38, 48),
+                (48, 49),
+                (49, 39),
+                (39, 33),
+                (33, 28) ]
+
+              # face 22
+
+              ]
+    return faces
+
+
+def edgeList():
+              # pentagram top
+    edges = [ (0, 1),
+              (1, 2),
+              (2, 3),
+              (3, 4),
+              (4, 0),
+              # top 5 verts
+              (0, 5),
+              (1, 6),
+              (2, 7),
+              (3, 8),
+              (4, 9),
+              # hexagon tops
+              (5, 10),
+              (5, 11),
+              (6, 12),
+              (6, 13),
+              (7, 14),
+              (7, 15),
+              (8, 16),
+              (8, 17),
+              (9, 18),
+              (9, 19),
+              # hexagon bottoms
+              (11, 12),
+              (13, 14),
+              (15, 16),
+              (17, 18),
+              (19, 10),
+              # pentagon bottom sides
+              (10, 20),
+              (11, 21),
+              (12, 22),
+              (13, 23),
+              (14, 24),
+              (15, 25),
+              (16, 26),
+              (17, 27),
+              (18, 28),
+              (19, 29),
+              # pentagon bottoms
+              (20, 21),
+              (22, 23),
+              (24, 25),
+              (26, 27),
+              (28, 29),
+              # hexagon side bottoms
+              (20, 30),
+              (21, 31),
+              (22, 32),
+              (23, 33),
+              (24, 34),
+              (25, 35),
+              (26, 36),
+              (27, 37),
+              (28, 38),
+              (29, 39),
+              # hexagon bottoms
+              (31, 32),
+              (33, 34),
+              (35, 36),
+              (37, 38),
+              (39, 30),
+              # hexagon bottom sides
+              (30, 40),
+              (31, 41),
+              (32, 42),
+              (33, 43),
+              (34, 44),
+              (35, 45),
+              (36, 46),
+              (37, 47),
+              (38, 48),
+              (39, 49),
+              # hexagon bottoms
+              (40, 41),
+              (42, 43),
+              (44, 45),
+              (46, 47),
+              (48, 49)
+    ]
+    return edges
+
+def dupEdges(verts, edges):
+    for edge in edges:
+        dupBetween(verts[edge[0]], verts[edge[1]])
+
+def makeEdgesFromFaces (verts, faces):
+    for face in faces:
+        for edge in face:
+            dupBetween(verts[edge[0]], verts[edge[1]])
+
+def dupEdges_old(verts):
     # face 1
     dupBetween(verts[0], verts[1])
     dupBetween(verts[1], verts[2])
@@ -308,10 +564,14 @@ def dupEdges(verts):
 
 def genMagetarium():
     # settings
-    edgeLength = 1 # in feet
-    verts = domeVerts(calcVerts(edgeLength/3.28084))
-    #dupVerts(verts)
-    dupEdges(verts)
+    edgeLength = 3 # in feet
+    verts = domeVerts_ccw(calcVerts(edgeLength/3.28084))[:50]
+    faces = faceList()
+    edges = edgeList()
+
+#    dupVerts(verts)
+    dupEdges(verts, edges)
+#    edgeList(verts, faces)
 
 if __name__ == "__main__":
     dupObject()
